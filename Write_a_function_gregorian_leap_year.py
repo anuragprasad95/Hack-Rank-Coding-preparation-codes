26496adc1714548f3d4d5561a58a826a945286a8
Anurag Prasad <er.anuragprasad@gmail.com>
2021-09-10 22:45:16 +0530

    Update Write_a_function_gregorian_leap_year.py

--- Write_a_function_gregorian_leap_year.py
+++ Write_a_function_gregorian_leap_year.py
@@ -1,13 +1,11 @@
 def is_leap(year):
     leap = False
-    if year%4==0:
+    if year%4==0 and (year%400==0 or year%100!=0):
         leap = True
-    elif year%100==0 and year%400==0:
-        leap = True    
+    elif year%100==0:
+            leap = False   
     else:
         leap = False     
-    # Write your logic here
-    
     return leap
 
 year = int(raw_input())
